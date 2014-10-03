807c69fe62634e1e6b607b098acbda9b1b937051
Sandro Simas <sandro.csimas@gmail.com>
2014-10-03 11:12:47 -0300

    Removing promessa-de-politico code from the project (2)

--- configs/routes.js
+++ configs/routes.js
@@ -8,6 +8,5 @@ exports.configure = function(express, app, passport) {
   require('../routes/music')(express.Router(), app);
   require('../routes/user')(express.Router(), app);
   require('../routes/search')(express.Router(), app);
-  require('../routes/upload')(express.Router(), app);
   require('../routes/index')(express.Router(), app);
 }
