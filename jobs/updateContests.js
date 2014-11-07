68f66aebdf72eb002e832c730b09507b429e29a8
Sandro Simas <sandro.csimas@gmail.com>
2014-11-07 01:14:18 -0300

    Updating jobs

---
+++ jobs/updateContests.js
@@ -0,0 +1,33 @@
+var contestService = require('../apis/contestService'),
+    Contest        = require('../models/models').Contest,
+    later          = require('later');
+
+var startContests = function() {
+  console.log('Starting contests');
+  contestService.listContestsToStart().then(function(contests) {
+    contests.forEach(function(contest) {
+      contestService.startContest(contest).then(function(contest) {
+        console.log('Contest ' + contest.id + ' started successfully');
+      }).catch(function(err) {
+        console.log('Error starting contest ' + contest.id + ': ' + err.message);
+      });
+    });
+  });
+}
+
+var finishContests = function() {
+  console.log('Finishing contests');
+  contestService.listContestsToFinish().then(function(contests) {
+    contests.forEach(function(contest) {
+      contestService.finishContest(contest).then(function() {
+        console.log('Contest ' + contest.id + ' was finished');
+      }).catch(function(err) {
+        console.log('Error finishing contest ' + contest.id + ': ' + err.message);
+      });
+    });
+  });
+}
+
+var sched = later.parse.recur().on(0).minute();
+var finishContestsInterval = later.setInterval(finishContests, sched);
+var startContestsInterval = later.setInterval(startContests, sched);
