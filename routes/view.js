63adf19338aa3d8c4aaa766bb19793e47c0c6c97
Sandro Simas <sandro.csimas@gmail.com>
2014-12-17 23:30:52 -0300

    Moving to the latest contest when access index

--- routes/view.js
+++ routes/view.js
@@ -61,7 +61,7 @@ module.exports = function(router, app) {
   router.get('/index', function(req, res, next) {
     contestService.listContests().then(function(contests) {
       if(contests.length > 0) {
-        messages.respondWithRedirection('contest', {id: contests.at(contests.length - 1).id, slug: contests.at(contests.length - 1).get('slug')}, res);
+        messages.respondWithRedirection('contest', {id: contests.at(0).id, slug: contests.at(0).get('slug')}, res);
       } else {
         messages.respondWithRedirection('covers', {}, res);
       }
